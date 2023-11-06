bc320e396c51619a94b044dd8485a37cc65b1844
Elyter <eliottbreg@gmail.com>
2023-11-06 15:20:04 +0100

    fix linux and darwin getip^

--- get_ip.py
+++ get_ip.py
@@ -1,4 +1,8 @@
 import psutil
+import platform
 
 def ip() :
-    return psutil.net_if_addrs()['en0'][0].address
+    if platform.system() == 'Linux' :
+        return psutil.net_if_addrs()['enp0s1'][1].address
+    elif platform.system() == 'Darwin' :
+        return psutil.net_if_addrs()['en0'][1].address
