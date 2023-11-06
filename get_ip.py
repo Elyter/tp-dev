fc0f081ef985f8a8edd2950143767b69c1029762
Elyter <eliottbreg@gmail.com>
2023-11-06 15:23:38 +0100

    fix getip linux

--- get_ip.py
+++ get_ip.py
@@ -3,6 +3,6 @@ import platform
 
 def ip() :
     if platform.system() == 'Linux' :
-        return psutil.net_if_addrs()['enp0s1'][1].address
+        return psutil.net_if_addrs()['enp0s1'][0].address
     elif platform.system() == 'Darwin' :
         return psutil.net_if_addrs()['en0'][1].address
